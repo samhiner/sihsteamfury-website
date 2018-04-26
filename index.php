a0ed71d138dbf5e1b6ac2582755f9ba7d5c171c4
smhnr27 <25464398+smhnr27@users.noreply.github.com>
2018-04-26 15:06:25 -0400

    Temp Index Page

---
+++ index.php
@@ -0,0 +1,101 @@
+<html>
+<!--
+ /$$$$$$$$                                      /$$$$$$$$                           
+|__  $$__/                                     | $$_____/                           
+   | $$  /$$$$$$   /$$$$$$  /$$$$$$/$$$$       | $$    /$$   /$$  /$$$$$$  /$$   /$$
+   | $$ /$$__  $$ |____  $$| $$_  $$_  $$      | $$$$$| $$  | $$ /$$__  $$| $$  | $$
+   | $$| $$$$$$$$  /$$$$$$$| $$ \ $$ \ $$      | $$__/| $$  | $$| $$  \__/| $$  | $$
+   | $$| $$_____/ /$$__  $$| $$ | $$ | $$      | $$   | $$  | $$| $$      | $$  | $$
+   | $$|  $$$$$$$|  $$$$$$$| $$ | $$ | $$      | $$   |  $$$$$$/| $$      |  $$$$$$$
+   |__/ \_______/ \_______/|__/ |__/ |__/      |__/    \______/ |__/       \____  $$
+                                                                           /$$  | $$
+                                                                          |  $$$$$$/
+                                                                           \______/ 
+-->
+<head>
+<link href="https://fonts.googleapis.com/css?family=Audiowide" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css?family=Alfa+Slab+One" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
+<style>
+h1 {
+	font-family: 'Audiowide', serif;
+}
+.center {
+	display: block;
+	margin-left: auto;
+	margin-right: auto;
+	padding: 0;
+	text-align: center;
+}
+p {
+	font-family: 'Alfa Slab One', serif;
+	color: #ff5722;
+	font-size: 30px;
+}
+ul {
+	color: #666666;
+	font-size: 20px;
+	font-family: 'Montserrat', serif;
+}
+.border {
+	margin-top: 50px;
+	margin-left: 100px;
+	margin-right: 100px;
+	padding: 50;
+	padding-top: 15;
+	background-color: E1E1E1;
+}
+</style></head><body>
+
+<div class='center'>
+	<h1>SIHS Team Fury</h1>
+	<img src='images/teamfurylogo.png' alt='SIHS Team Fury Logo' width='337' height='200' class='imgCenter'>
+</div>
+<div class='border'>
+	<p>Our Members</p>
+	<ul class="font">
+		<li>Martin Blue</li>
+		<li>Indra Gottiveeti</li> 
+		<li>AJ Valentino</li>
+		<li>Pranav Swarna</li>
+		<li>Ahobil Thuppal</li>
+		<li>Sam Hiner</li>
+		<li>Breanna Nguyen</li>
+		<li>Foluwa Agbebi</li>
+		<li>Ian Loh</li>
+		<li>Emeline Gilbert</li>
+		<li>Julia Mcclean</li>
+	</ul>
+	<p>FIRST Organization</p>
+	<ul>
+		<li>Operates FIRST Robotics Competition</li>
+		<li>Real world engineering</li>
+		<li><a href="https://www.firstinspires.org/robotics/ftc">FIRST Website</a></li>
+	</ul>
+	<p>Software Development Team</p>
+	<ul>
+		<li>Build a website using HTML/CSS and Javascript</li>
+		<li>Program a robot using Java</li>
+	</ul>
+	<p>Marketing/Production Team</p>
+	<ul>
+		<li>Sponsors & Outreach</li>
+		<li>Marketing</li>
+		<li>Social Media</li>
+		<li>Scheduling</li>
+	</ul>
+	<p>Management Team</p>
+	<ul>
+		<li>Engineering Notebook</li>
+		<li>Manages Tasks and Deadlines</li>
+		<li>Sets Objectives</li>
+		<li>Creates Presentations</li>
+	</ul>
+	<p>Engineering Team</p>
+	<ul>
+		<li>CAD Design</li>
+		<li>Technical Presentation</li>
+		<li>Physically Building the Robot</li>
+	</ul>
+</div>
+</body></html>
