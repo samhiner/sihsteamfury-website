44c465da26332bfae74adb4eb74fd29aa63055da
bluebotz <12171023@57276.local>
2018-04-27 08:48:33 -0400

    Separated html & css
    Code cleanup

--- index.php
+++ index.php
@@ -13,140 +13,83 @@
                                                                            \______/
 -->
 <head>
-<link href="https://fonts.googleapis.com/css?family=Audiowide" rel="stylesheet">
-<link href="https://fonts.googleapis.com/css?family=Alfa+Slab+One" rel="stylesheet">
-<link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
-<title>Homepage</title>
+	<link href="https://fonts.googleapis.com/css?family=Audiowide" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Alfa+Slab+One" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
+	<title>Homepage</title>
+	<link rel="stylesheet" type="text/css" href="main.css">
 </head>
-<style>
-h1 {
-	font-family: 'Audiowide', serif;
-}
-.center {
-	display: block;
-	margin-left: auto;
-	margin-right: auto;
-	padding: 0;
-	text-align: center;
-}
-p {
-	font-family: 'Alfa Slab One', serif;
-	color: #ff5722;
-	font-size: 30px;
-}
-ul {
-	color: #666666;
-	font-size: 20px;
-	font-family: 'Montserrat', serif;
-}
-.border {
-	margin-left: 100px;
-	margin-right: 100px;
-	padding: 50;
-	padding-top: 15;
-	background-color: E1E1E1;
-	border-style: solid;
-	border-color: 2d5ca9;
-}
-.tabArea {
-	margin-top: 50px;
-	margin-left: 100px;
-	margin-right: 100px;
-}
-.tabArea input {
-    width: 33.333333%;
-    float: left;
-	height: 55px;
-	font-family: 'Audiowide', serif;
-	font-size: 30px;
-	background-color: 4285f4;
-	border-style: solid;
-	border-color: 2d5ca9;
-	cursor:pointer;
-}
-form {
-	margin-bottom: 0px;
-}
-.clearFix{
-    clear:both
-}
-.first {
-	border-width: 6px 3px 6px 6px;
-	border-top-left-radius: 10px
-}
-.mid {
-	border-width: 6px 3px 6px 3px;
-}
-.last {
-	border-width: 6px 6px 6px 3px;
-	border-top-right-radius: 10px
-}
-</style></head><body>
 
-<div class='center'>
-	<h1>SIHS Team Fury</h1>
-	<img src='images/teamfurylogo.png' alt='SIHS Team Fury Logo' width='337' height='200'>
-</div>
+<body>
+	<div class='center'>
+		<h1>SIHS Team Fury</h1>
+		<img src='images/teamfurylogo.png' alt='SIHS Team Fury Logo' width='337' height='200'>
+	</div>
 
-<div class='tabArea'>
-	<form method='post'>
-		<input type='submit' value='Home' name='home' class='first'>
-		<input type='submit' value='Elections' name='elect' class='mid'>
-		<input type='submit' value='Contact Us' name='contact' class='last'>
-	</form>
-	<div class="clearFix"></div>
-</div>
+	<div class='tabArea'>
+		<form method='post'>
+			<input type='submit' value='Home' name='home' class='first'>
+			<input type='submit' value='Elections' name='elect' class='mid'>
+			<input type='submit' value='Contact Us' name='contact' class='last'>
+		</form>
+		<div class="clearFix"></div>
+	</div>
 
-<div class='border'>
-	<p>Our Members</p>
-	<ul class="font">
-		<li>Martin Blue</li>
-		<li>Indra Gottiveeti</li>
-		<li>AJ Valentino</li>
-		<li>Pranav Swarna</li>
-		<li>Ahobil Thuppal</li>
-		<li>Sam Hiner</li>
-		<li>Breanna Nguyen</li>
-		<li>Foluwa Agbebi</li>
-		<li>Ian Loh</li>
-		<li>Emeline Gilbert</li>
-		<li>Julia Mcclean</li>
-	</ul>
-	<p>FIRST Organization</p>
-	<ul>
-		<li>Operates FIRST Robotics Competition</li>
-		<li>Real world engineering</li>
-		<li><a href="https://www.firstinspires.org/robotics/ftc">FIRST Website</a></li>
-	</ul>
-	<p>Software Development Team</p>
-	<ul>
-		<li>Build a website using HTML/CSS and Javascript</li>
-		<li>Program a robot using Java</li>
-	</ul>
-	<p>Marketing/Production Team</p>
-	<ul>
-		<li>Sponsors & Outreach</li>
-		<li>Marketing</li>
-		<li>Social Media</li>
-		<li>Scheduling</li>
-	</ul>
-	<p>Management Team</p>
-	<ul>
-		<li>Engineering Notebook</li>
-		<li>Manages Tasks and Deadlines</li>
-		<li>Sets Objectives</li>
-		<li>Creates Presentations</li>
-	</ul>
-	<p>Engineering Team</p>
-	<ul>
-		<li>CAD Design</li>
-		<li>Technical Presentation</li>
-		<li>Physically Building the Robot</li>
-	</ul>
-</div>
+	<div class='border'>
+		<p>Our Members</p>
+		<ul class="font">
+			<li>Martin Blue</li>
+			<li>Indra Gottiveeti</li>
+			<li>AJ Valentino</li>
+			<li>Pranav Swarna</li>
+			<li>Ahobil Thuppal</li>
+			<li>Sam Hiner</li>
+			<li>Breanna Nguyen</li>
+			<li>Foluwa Agbebi</li>
+			<li>Ian Loh</li>
+			<li>Emeline Gilbert</li>
+			<li>Julia Mcclean</li>
+		</ul>
 
+		<p>FIRST Organization</p>
+		<ul>
+			<li>Operates FIRST Robotics Competition</li>
+			<li>Real world engineering</li>
+			<li><a href="https://www.firstinspires.org/robotics/ftc">FIRST Website</a></li>
+		</ul>
+
+		<p>Software Development Team</p>
+		<ul>
+			<li>Build a website using HTML/CSS and Javascript</li>
+			<li>Program a robot using Java</li>
+		</ul>
+
+		<p>Marketing/Production Team</p>
+		<ul>
+			<li>Sponsors & Outreach</li>
+			<li>Marketing</li>
+			<li>Social Media</li>
+			<li>Scheduling</li>
+		</ul>
+
+		<p>Management Team</p>
+		<ul>
+			<li>Engineering Notebook</li>
+			<li>Manages Tasks and Deadlines</li>
+			<li>Sets Objectives</li>
+			<li>Creates Presentations</li>
+		</ul>
+
+		<p>Engineering Team</p>
+		<ul>
+			<li>CAD Design</li>
+			<li>Technical Presentation</li>
+			<li>Physically Building the Robot</li>
+		</ul>
+	</div>
 </body>
 </html>
+
 <?php
 
 if(isset($_POST[elect])) {
